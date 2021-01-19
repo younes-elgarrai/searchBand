a5793578f74f775fabd049fe3db3c820d015121e
Younes <yelgarrai@gmail.com>
2021-01-20 00:07:33 +0100

    add API request

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,59 @@ router.get('/', function(req, res, next) {
 router.post('/search',function(req, res, next){
 
   var url = "https://musicbrainz.org/ws/2/artist?query="+req.body.artist+"&fmt=json&limit=10"
-  var response = request('GET', url);
+  var response = request('GET', url, {
+    headers: {
+      'user-agent': 'searchBand/1.0.0',
+    },
+  });
+
   response = JSON.parse(response.getBody());
 
-  res.send(response);
+  // recuperation des ids :
+
+  var artists = response.artists ;
+
+  var idTable = [];
+  var nameTable = [];
+
+  for (let index = 0; index < artists.length; index++) {
+    idTable.push(
+      artists[index].id
+    );
+    nameTable.push(
+      artists[index].name
+    )
+  }
+
+  res.render('artists',{nameTable : nameTable, idTable: idTable});
+
+
+});
+
+router.get('/releases',function(req,res,next){
+
+  var id = req.query.id;
+  var url = "https://musicbrainz.org/ws/2/artist/"+id+"?inc=releases&fmt=json"
+  var response = request('GET', url, {
+    headers: {
+      'user-agent': 'searchBand/1.0.0',
+    },
+  });
+
+  response = JSON.parse(response.getBody());
+
+
+  var releases = response.releases ;
+
+  var releasesTable = [];
+
+  for (let index = 0; index < releases.length; index++) {
+    releasesTable.push(
+      releases[index].title
+    )
+  }
+  
+  res.render('releases', {releasesTable: releasesTable});
 
 
 });
